04beb3726f73d5d3708668e65b3f2b25d53a8ae3
Ahyas Widyatmaka <ahyasw@gmail.com>
2021-11-25 15:05:24 +0900

    Adding login/register mechanism

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get("home","HomeController@index")->name("home.index");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
